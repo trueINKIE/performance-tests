177b64e1d093600b7ff33c53581628a779e49340
trueINKIE <82120606+trueINKIE@users.noreply.github.com>
2025-09-01 18:55:55 +0300

    Практикуемся в использовании HTTP API клиентов. Практика использования HTTP API-клиентов: создание операции пополнения счета

--- api_client_make_top_up_operation.py
+++ api_client_make_top_up_operation.py
@@ -1,10 +1,8 @@
 from clients.http.gateway.accounts.client import build_accounts_gateway_http_client
-from clients.http.gateway.cards.client import build_cards_gateway_http_client
 from clients.http.gateway.operations.client import build_operations_gateway_http_client
 from clients.http.gateway.users.client import build_users_gateway_http_client
 
 users_gateway_client = build_users_gateway_http_client()
-cards_gateway_client = build_cards_gateway_http_client()
 accounts_gateway_client = build_accounts_gateway_http_client()
 operations_gateway_client = build_operations_gateway_http_client()
 

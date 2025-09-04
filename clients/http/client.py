a187d4ce09c78e6bbfef90e6a60aefabdebedaf2
trueINKIE <82120606+trueINKIE@users.noreply.github.com>
2025-09-04 18:21:44 +0300

    Использование HTTP API клиентов в нагрузочных сценариях. Практика: Применение HTTP API клиентов в нагрузочном сценарии

--- clients/http/client.py
+++ clients/http/client.py
@@ -7,6 +7,10 @@ class HTTPClientExtension(TypedDict, total=False):
     route: str
 
 
+class HTTPClientExtensions(TypedDict, total=False):
+    route: str
+
+
 class HTTPClient:
     """
     Базовый HTTP API клиент, принимающий объект httpx.Client.
@@ -14,37 +18,37 @@ class HTTPClient:
     :param client: экземпляр httpx.Client для выполнения HTTP-запросов
     """
 
-    def __init__(self, client: Client):
+    def __init__(self, client: Client) -> None:
         self.client = client
 
     def get(
             self,
-            url: URL | str,
+            url: str | URL,
             params: QueryParams | None = None,
-            extensions: HTTPClientExtension | None = None
+            extensions: HTTPClientExtensions | None = None
     ) -> Response:
         """
         Выполняет GET-запрос.
 
         :param url: URL-адрес эндпоинта.
         :param params: GET-параметры запроса (например, ?key=value).
-        :param extensions: Дополнительные данные передаваемые через HTTPX extensions.
+        :param extensions: Дополнительные данные, передаваемые через HTTPX extensions.
         :return: Объект Response с данными ответа.
         """
-        return self.client.get(url, params=params)
+        return self.client.get(url=url, params=params, extensions=extensions)
 
     def post(
             self,
-            url: str,
+            url: str | URL,
             json: Any | None = None,
-            extensions: HTTPClientExtension | None = None
+            extensions: HTTPClientExtensions | None = None
     ) -> Response:
         """
         Выполняет POST-запрос.
 
         :param url: URL-адрес эндпоинта.
         :param json: Данные в формате JSON.
-        :param extensions: Дополнительные данные передаваемые через HTTPX extensions.
+        :param extensions: Дополнительные данные, передаваемые через HTTPX extensions.
         :return: Объект Response с данными ответа.
         """
-        return self.client.post(url=url, json=json)
+        return self.client.post(url=url, json=json, extensions=extensions)
--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -1,27 +1,28 @@
-from locust import HttpUser, between, task
+from locust import between, task, User
 
-from tools.fakers import fake
+from clients.http.gateway.accounts.client import build_accounts_gateway_locust_http_client, AccountsGatewayHTTPClient
+from clients.http.gateway.accounts.schema import OpenDebitCardAccountResponseSchema
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 
 
-class GetUserScenarioUser(HttpUser):
+class GetUserScenarioUser(User):
+    host = "localhost"
     wait_time = between(1, 3)
-    user_data: dict
+
+    users_gateway_client: UsersGatewayHTTPClient
+    accounts_gateway_client: AccountsGatewayHTTPClient
+    create_user_response: CreateUserResponseSchema
+    open_debit_card_account_response: OpenDebitCardAccountResponseSchema
 
     def on_start(self) -> None:
         """
         Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
         Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
         """
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
-
-        self.user_data = response.json()
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
+        self.accounts_gateway_client = build_accounts_gateway_locust_http_client(self.environment)
+        self.create_user_response = self.users_gateway_client.create_user()
 
     @task
     def open_debit_card_account(self):
@@ -29,7 +30,6 @@ class GetUserScenarioUser(HttpUser):
         Основная нагрузочная задача: открытие дебетового теста.
         Здесь мы выполняем POST-запрос к /api/v1/accounts/open-debit-card-account.
         """
-        request = {
-            "userId": self.user_data['user']['id']
-        }
-        self.client.post("/api/v1/accounts/open-debit-card-account", json=request)
+        self.open_debit_card_account_response = self.accounts_gateway_client.open_debit_card_account(
+            user_id=self.create_user_response.user.id
+        )

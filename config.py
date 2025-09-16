cb79681861ffa48ac153de9406abe9e5f25b6b63
trueINKIE <82120606+trueINKIE@users.noreply.github.com>
2025-09-16 12:00:50 +0300

    Практика: загрузка данных в Load Testing Hub. Практика: загрузка результатов в сервис Load Testing Hub для HTTP-сценариев

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
+import locust.stats
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from tools.config.grpc import GRPCClientConfig
 from tools.config.http import HTTPClientConfig
 from tools.config.locust import LocustUserConfig
+
+
+locust.stats.PERCENTILES_TO_REPORT = [0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99, 1.0]
+locust.stats.CSV_STATS_INTERVAL_SEC = 5
+locust.stats.HISTORY_STATS_INTERVAL_SEC = 5
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 5
+locust.stats.CSV_STATS_FLUSH_INTERVAL_SEC = 5
 
 
 class Settings(BaseSettings):
